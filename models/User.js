d2d6b3b81755b689b05371d676e2398615e80369
Jack Gray <jack.odg@outlook.com>
2020-06-15 15:31:05 +0100

    added user registration, validation, bcrypt

--- models/User.js
+++ models/User.js
@@ -1,11 +1,6 @@
 const mongoose = require('mongoose');
 
 const UserSchema = new mongoose.Schema({
-	username: {
-		type: String,
-		lowercase: true,
-		unique: true
-	},
 	email: {
 		type: String,
 		required: true,
--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,42 @@
 const router = require('express').Router();
+const bcrypt = require('bcryptjs');
 const User = require('../models/User');
 
-// @route POST /api/users */
+// @route POST /api/users
 // @desc register a new user
 // @access public
 router.post('/', (req, res) => {
-	res.json({ message: "register" })
+	const { email, password, passwordConfirm } = req.body;
+	// all fields must be entered
+	if (!email || !password || !passwordConfirm) {
+		return res.status(400).json({ message: 'Please enter all fields.' });
+	}
+	// passwords must match
+	if (password !== passwordConfirm) {
+		return res.status(400).json({ message: 'Passwords must match.' })
+	}
+
+	// check for duplicates
+	User.findOne({ email })
+		.then(user => {
+			if (user) return res.status(400).json({ message: 'A user with that email already exists.' });
+			// if no duplicates, register
+			const newUser = new User({
+				email,
+				password
+			});
+
+			//create salt and hash
+			bcrypt.genSalt(12, (err, salt) => {
+				bcrypt.hash(newUser.password, salt, (err, hash) => {
+					if (err) throw err;
+					newUser.password = hash;
+					newUser.save().then(user => {
+						res.status(200).json({ user: { id: user._id, email: user.email } })
+					})
+				})
+			})
+		})
 });
 
 module.exports = router
