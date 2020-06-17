a93622adb413d02e46437d674ee9105ac2c7b72d
Jack Gray <jack.odg@outlook.com>
2020-06-17 14:17:57 +0100

    dashboard menu styling

--- client/src/components/layout/admin/AdminHeader.js
+++ client/src/components/layout/admin/AdminHeader.js
@@ -8,24 +8,31 @@ const Header = () => {
 		<aside className="admin-menu">
 			<h1 className="admin-menu__title">Blue Shade</h1>
 			<ul className="admin-menu__links">
-				<li>Lorem</li>
+				<li className="admin-menu__title"><i className="fas fa-shoe-prints"></i>Shoes</li>
 				<li>ipsum</li>
 				<li>dolor</li>
 				<li>sit</li>
 				<li>amet</li>
 			</ul>
 			<ul className="admin-menu__links">
-				<li>Lorem</li>
+				<li className="admin-menu__title"><i className="fas fa-blog"></i>Blog</li>
+				<li>ipsum</li>
+				<li>dolor</li>
+				<li>sit</li>
+				<li>amet</li>
+			</ul>
+			<ul className="admin-menu__links">
+				<li className="admin-menu__title"><i className="fas fa-file"></i>Pages</li>
 				<li>ipsum</li>
 				<li>dolor</li>
 				<li>sit</li>
 				<li>amet</li>
 			</ul>
 			<ul className="admin-menu__links account-links">
-				<li>My Account</li>
-				<li onClick={() => { auth.ACTIONS.logout(auth.dispatch) }}><span>Logout</span></li>
+				<li><i className="fas fa-user-cog"></i>My Account</li>
+				<li onClick={() => { auth.ACTIONS.logout(auth.dispatch) }}><i className="fas fa-sign-out-alt"></i>Logout</li>
 			</ul>
-		</aside>
+		</aside >
 	)
 }
 
