e606d4d60f7aac9236e4cd4ef0e046943b8c800f
Jack Gray <jack.odg@outlook.com>
2020-07-01 15:19:03 +0100

    mobile menu added

--- client/src/components/layout/Header.js
+++ client/src/components/layout/Header.js
@@ -1,27 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import Logo from '../../media/logo83x192.png';
 
 const Header = () => {
+	const [state, setState] = useState({
+		menuVisible: false
+	})
+	const toggleMenu = () => {
+		setState({ ...state, menuVisible: !state.menuVisible })
+	}
+	const hideMenu = () => {
+		setState({ ...state, menuVisible: false })
+	}
 	return (
 		<header className="header">
 			<div className="header__logo">
 				<img src={Logo} alt="logo" />
 			</div>
 			<div className="header__navigation">
-				<nav>
+				<div role="button" onClick={toggleMenu} className={state.menuVisible ? "hamburger hidden" : "hamburger"}>
+					<i className="fas fa-bars"></i>
+				</div>
+				<nav className={state.menuVisible ? "" : "hidden"} >
+					<div role="button" onClick={toggleMenu} id="close-menu">&times;</div>
 					<ul className="nav">
 						<li className="nav-item">
-							<Link to="/">Home</Link>
+							<Link onClick={hideMenu} to="/">Home</Link>
 						</li>
 						<li className="nav-item">
-							<Link to="/shop">Shop</Link>
+							<Link onClick={hideMenu} to="/shop">Shop</Link>
 						</li>
 						<li className="nav-item">
-							<Link to="/blog">Blog</Link>
+							<Link onClick={hideMenu} to="/blog">Blog</Link>
 						</li>
 						<li className="nav-item">
-							<Link to="/contact">Contact</Link>
+							<Link onClick={hideMenu} to="/contact">Contact</Link>
 						</li>
 					</ul>
 				</nav>
